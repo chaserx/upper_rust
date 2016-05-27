7883953723f525a7a3a278e8eda6207a95fd0d5b
Chase Southard <chase.southard@gmail.com>
2016-05-27 13:42:24 -0400

    move into check_url function with arguments

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,12 @@ use std::io::Read;
 use hyper::{Client};
 use hyper::header::{Authorization};
 
-fn main() {
-    dotenv().ok();
-
+// probably should make this return a boolean and take URL and AUTH arguments
+fn check_url(uri: String, auth: String) {
     let client = Client::new();
-    let uri = env::var("URL").unwrap();
-    let auth_token = env::var("AUTH").unwrap();
-
     println!("Getting... {}", uri);
     let mut res = client.get(&uri)
-                        .header(Authorization(auth_token))
+                        .header(Authorization(auth))
                         .send()
                         .unwrap();
 
@@ -24,4 +20,12 @@ fn main() {
     let mut s = String::new();
     res.read_to_string(&mut s).unwrap();
     println!("{:?}", s);
+}
+
+fn main() {
+    dotenv().ok();
+
+    let uri = env::var("URL").unwrap();
+    let auth_token = env::var("AUTH").unwrap();
+    check_url(uri, auth_token);
 }
