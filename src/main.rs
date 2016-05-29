cd7498043881d53dc49d5433ea7cf75a55a81374
Chase Southard <chase.southard@gmail.com>
2016-05-29 15:15:22 -0400

    refactored; still works

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,51 @@ use std::env;
 use std::io::Read;
 use hyper::{Client};
 use hyper::header::{Authorization};
+use std::time::{Duration};
+use std::thread::sleep;
 
-// probably should make this return a boolean
-fn check_url(uri: String, auth: String) {
+fn check_url(uri: &str, auth_token: &str) -> bool {
     let client = Client::new();
     println!("Getting... {}", uri);
-    let mut res = client.get(&uri)
-                        .header(Authorization(auth))
+    let mut res = client.get(uri)
+                        .header(Authorization(auth_token.to_owned()))
                         .send()
                         .unwrap();
-
-    assert_eq!(res.status, hyper::Ok);
-    let mut s = String::new();
-    res.read_to_string(&mut s).unwrap();
-    println!("{:?}", s);
+    // probably need to switch this to a match statement to capture when DNS fails
+    // or myabe use the try! macro
+    if hyper::Ok == res.status {
+            let mut s = String::new();
+            res.read_to_string(&mut s).unwrap();
+        if String::from(env::var("VERBOSE_MODE").unwrap()) == "true" {
+            println!("{:?}", s);
+        }
+        return true
+    } else {
+        return false;
+    }
 }
+
+// another function that will alert someone, maybe slack webhook.
+// fn notify() {
+
+// }
 
 fn main() {
     dotenv().ok();
 
-    let uri = env::var("URL").unwrap();
-    let auth_token = env::var("AUTH").unwrap();
-    check_url(uri, auth_token);
+    let uri = String::from(env::var("URL").unwrap());
+    let auth_token = String::from(env::var("AUTH").unwrap());
+    let mut attempts = 0;
+    let max_attempts = 5;
+
+    while !check_url(&uri, &auth_token) {
+        println!("site not responding, rechecking...");
+        sleep(Duration::from_secs(60));
+        attempts += 1;
+        if attempts >= max_attempts {
+            // notify
+            println!("bummer");
+            break;
+        }
+    }
 }
